8df868e8f098953034fa2fb35acc45d08ad1c5d5
steven <steven.araujo@hotmail.com>
2018-08-19 18:49:03 -0500

    Perfiles

---
+++ app/src/main/java/proyecto/mundo/hola/ssam/actividadesespol/PerfilFotografia.java
@@ -0,0 +1,13 @@
+package proyecto.mundo.hola.ssam.actividadesespol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilFotografia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_fotografia);
+    }
+}
---
+++ app/src/main/java/proyecto/mundo/hola/ssam/actividadesespol/PerfilTaws.java
@@ -0,0 +1,13 @@
+package proyecto.mundo.hola.ssam.actividadesespol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilTaws extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_taws);
+    }
+}
